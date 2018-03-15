dbb9fc1be6c73617c462e4e3a763802c9f5d4ba5
Chris <chrischuck35@gmail.com>
2018-03-14 23:37:57 -0700

    Adds admin table

--- api/src/app/index.js
+++ api/src/app/index.js
@@ -5,49 +5,10 @@ import url from 'url'
 
 import pool from '../db'
 
+import { toppings, sizes, admin } from '../data'
+
 const PORT = 3000
 const app = express()
-
-const toppings = [
-  { 
-    name: 'Cheese',
-    value: 'CHEESE',
-    price: 1.25
-  },
-  {
-    name: 'Mushrooms',
-    value: 'MUSHRROMS',
-    price: 1.25
-  },
-  {
-    name: 'Pepperoni',
-    value: 'PEPPERONI',
-    price: 1.00
-  },
-  {
-    name: 'Bell Peppers',
-    value: 'BELL_PEPPERS',
-    price: 2.00
-  }
-]
-
-const sizes = [
-  { 
-    name: 'Small',
-    value: 'SMALL',
-    price: 10.00
-  },
-  {
-    name: 'Medium',
-    value: 'MEDIUM',
-    price: 12.00
-  },
-  {
-    name: 'Large',
-    value: 'LARGE',
-    price: 15.00
-  }
-]
 
 
 pool.getConnection((err, connection) => {  
@@ -61,11 +22,19 @@ pool.getConnection((err, connection) => {
     if (error) throw error;
   });
 
+  connection.query('DROP TABLE IF EXISTS `admin`;', (error, results, fields) => {
+    if (error) throw error;
+  });
+
   connection.query('CREATE TABLE `toppings` (`name` VARCHAR(20), `value` VARCHAR(20), `price` DECIMAL(13,2));', (error, results, fields) => {
     if (error) throw error;
   });
 
   connection.query('CREATE TABLE `sizes` (`name` VARCHAR(20), `value` VARCHAR(20), `price` DECIMAL(13,2));', (error, results, fields) => {
+    if (error) throw error;
+  });
+
+  connection.query('CREATE TABLE `admin` (`username` VARCHAR(20), `password` VARCHAR(20));', (error, results, fields) => {
     if (error) throw error;
   });
 
@@ -81,6 +50,11 @@ pool.getConnection((err, connection) => {
     connection.query('INSERT INTO `sizes` (`name`, `value`, `price`) ' + values, (error, results, fields) => {
       if (error) throw error;
     });
+  });
+
+  const adminValues = `VALUES ('${admin.username}', '${admin.password}');`
+  connection.query('INSERT INTO `admin` (`username`, `password`) ' + adminValues, (error, results, fields) => {
+    if (error) throw error;
   });
 
   connection.release();
---
+++ api/src/data/index.js
@@ -0,0 +1,45 @@
+export const toppings = [
+  { 
+    name: 'Cheese',
+    value: 'CHEESE',
+    price: 1.25
+  },
+  {
+    name: 'Mushrooms',
+    value: 'MUSHRROMS',
+    price: 1.25
+  },
+  {
+    name: 'Pepperoni',
+    value: 'PEPPERONI',
+    price: 1.00
+  },
+  {
+    name: 'Bell Peppers',
+    value: 'BELL_PEPPERS',
+    price: 2.00
+  }
+]
+
+export const sizes = [
+  { 
+    name: 'Small',
+    value: 'SMALL',
+    price: 10.00
+  },
+  {
+    name: 'Medium',
+    value: 'MEDIUM',
+    price: 12.00
+  },
+  {
+    name: 'Large',
+    value: 'LARGE',
+    price: 15.00
+  }
+]
+
+export const admin = {
+  user: 'user',
+  password: 'hunter2'
+}
