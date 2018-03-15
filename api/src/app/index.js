ae5cd72a93b437920ed83cf7b8c8d457e10e66a8
Chris <chrischuck35@gmail.com>
2018-03-14 22:03:54 -0700

    Adds basic express backend

---
+++ api/src/app/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import url from 'url'
+
+const PORT = 3000
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => res.status(200).send('200 OK'))
+
+// Creating/getting orders
+app.get('/order', (req, res) => res.status(200).send('200 OK'))
+app.post('/order', (req, res) => res.status(200).send('200 OK'))
+
+// Editing/Making/Getting toppigns
+app.get('/toppings', (req, res) => res.status(200).send('200 OK'))
+app.post('/toppings', (req, res) => res.status(200).send('200 OK'))
+
+
+// AUTH for admin
+app.post('/login', (req, res) => res.status(200).send('200 OK'))
+
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}!`))
